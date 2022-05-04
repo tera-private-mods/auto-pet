84c5c729d0167f8cb27cbf052fab49969745f6e5
Dmitry S <dmitry.s@hsdn.org>
2022-05-05 00:26:27 +0400

    p92

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-## Info:
+# auto-pet
 
-- Automatically summons and feeds your pet. /// Only that work 
-- Automatically uses Bracing Force pet buff when using your brooch. /// Not since 97 patch
+- Automatically summons and feeds your pet.
+- Automatically uses Bracing Force pet buff when using your brooch.
 
----
-
-## Commands:
+## Commands
 
 - **pet**
   - to toggle ON/OFF (Default ON)
@@ -18,12 +16,3 @@
   - to turn module ON for the character currently logged in
 - **pet off**
   - to turn module OFF for the character currently logged in
-- **pet bond**
-  - use your pet's skill after using this command to save it to config
-- **pet delay [#]**
-  - pet buff activation delay in milliseconds (ms)
-  - Example: **pet delay 1000** will delay the pet buff by 1000ms (1 sec) after brooch
-
----
-
-Patch Version: **90.2**
--- settings_migrator.js
+++ settings_migrator.js
@@ -1,29 +1,29 @@
 const DefaultSettings = {
-	enabled: true,
-	petFood: [
+	"enabled": true,
+	"petFood": [
 		{
-		id: 206049,
-		name: "Puppy Figurine",
-		cd: 2,
-	},
-	{
-		id: 206050,
-		name: "Piglet Figurine",
-		cd: 2,
-	},
-	{
-		id: 206051,
-		name: "Popori Figurine",
-		cd: 2,
-	}],
-	feedWhenBelow: 90,
-	servantsList: {},
-	characters: {}
+			"id": 206049,
+			"name": "Puppy Figurine",
+			"cd": 2
+		},
+		{
+			"id": 206050,
+			"name": "Piglet Figurine",
+			"cd": 2
+		},
+		{
+			"id": 206051,
+			"name": "Popori Figurine",
+			"cd": 2
+		}],
+	"feedWhenBelow": 90,
+	"servantsList": {},
+	"characters": {}
 };
 
 module.exports = function MigrateSettings(from_ver, to_ver, settings) {
 	if (from_ver === undefined)
-		return Object.assign(Object.assign({}, DefaultSettings), settings);
+		return { ...DefaultSettings, ...settings };
 	else if (from_ver === null) return DefaultSettings;
 	else {
 		if (from_ver + 1 < to_ver) {
